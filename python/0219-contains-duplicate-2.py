dbcc7d7e5868253f58913949aaccf5295dba45a4
Brian Kam <brianhyunkam@gmail.com>
2024-12-18 11:29:28 -0800

    219

--- python/0219-contains-duplicate-2.py
+++ python/0219-contains-duplicate-2.py
@@ -11,3 +11,15 @@ class Solution:
             hash_set.add(nums[right])
 
         return False
+
+#using hash_dict
+# time: O(N), #space: O(N)
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hash_dict = {}
+
+        for i, v in enumerate(nums):
+            if v in hash_dict and abs(i - hash_dict[v]) <= k:
+                return True
+            hash_dict[v] = i
+        return False
