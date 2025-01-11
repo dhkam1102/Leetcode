fe39a81e57eb67116903f74cab566820cce6fc10
Brian Kam <brianhyunkam@gmail.com>
2025-01-11 12:31:00 -0800

    learn

--- python/0001-two-sum.py
+++ python/0001-two-sum.py
@@ -7,3 +7,13 @@ class Solution:
         for i, v in enumerate(nums):
             if v in hash_map and hash_map[v] != i:
                 return [hash_map[v], i]
+            
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i, num in enumerate(nums):
+            if num in hash_map:
+                return [hash_map[num], i]
+            else:
+                hash_map[target - num] = i
+        return [-1, -1]
